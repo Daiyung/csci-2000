f3fdc4ca6e320d155e5aaafa6ee17275737863f8
Vincent Chan <vincent.d.chan@gmail.com>
2015-10-28 09:45:40 -0400

    Midterm: Committed question 5 and 6

---
+++ Midterm/has_no_e.py
@@ -0,0 +1,12 @@
+def has_no_e(string s)
+
+    for c in (s)
+        if s = 'e'
+            return True
+        else
+            return False
+
+    end for
+
+reader = open ('gadsby_stripped.txt')
+line = reader.readline()
---
+++ Midterm/strip.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#Dai Vincent Chan
+#100 554 446 
+
+echo "Enter number of lines to strip from the top of the file"
